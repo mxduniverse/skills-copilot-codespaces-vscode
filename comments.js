6f6ad5938cc97455b87ab11ac36b97768d3a0b25
mxduniverse <57356789+mxduniverse@users.noreply.github.com>
2023-07-30 12:56:43 +0000

    fix selection another solution

--- comments.js
+++ comments.js
@@ -1,32 +1,52 @@
-// create web server
-// run node comments.js
-// open browser: http://localhost:3000
-// open another browser: http://localhost:3000
-// use different browsers to test
+// create web server with express
+// run with node comments.js
+// open browser and go to http://localhost:8080
 
-var http = require('http');
-var items = []; // store data in memory
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Comment = require('./models/comment');
 
-var server = http.createServer(function(req, res){
-	switch (req.method) {
-		case 'POST':
-			var item = '';
-			req.setEncoding('utf8');
-			req.on('data', function(chunk){
-				item += chunk;
-			});
-			req.on('end', function(){
-				items.push(item);
-				res.end('OK\n');
-			});
-			break;
-		case 'GET':
-			items.forEach(function(item, i){
-				res.write(i + ') ' + item + '\n');
-			});
-			res.end();
-			break;
-	}
+var db = 'mongodb://localhost/example';
+mongoose.connect(db);
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+// root route
+app.get('/', function(req, res) {
+    res.render('home');
 });
 
-server.listen(3000);
+// index route
+app.get('/comments', function(req, res) {
+    Comment.find({}, function(err, comments) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('comments/index', {comments: comments});
+        }
+    });
+});
+
+// new route
+app.get('/comments/new', function(req, res) {
+    res.render('comments/new');
+});
+
+// create route
+app.post('/comments', function(req, res) {
+    Comment.create(req.body.comment, function(err, newComment) {
+        if (err) {
+            res.render('comments/new');
+        } else {
+            res.redirect('/comments');
+        }
+    });
+});
+
+app.listen(8080, function() {
+    console.log('server started');
+});
